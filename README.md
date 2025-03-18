badd3ecfb3abbde96a8e22ee9acd0f258035bd20
Jose <jguerrah@uni.pe>
2025-03-18 08:41:41 -0500

    last updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-New site
+# Wedding Website
+
+Website de mi boda, inspirado en este _(template)[https://invitacion-amorosa.queridaboda.com/]_
+
+# Highlights
+
+- Creado sin un orden en particular
+- Código desordenado, no sugiero tomarlo de referencia
+- Repo se volverá privado después de la boda
--- scripts.js
+++ scripts.js
@@ -1,29 +1,3 @@
-$(document).ready(function () {
-    /********************** RSVP **********************/
-    $('#rsvp-form').on('submit', function (e) {
-        e.preventDefault();
-        var data = $(this).serialize();
-
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Un segundo!</strong> Estamos guardando los detalles.'));
-
-        $.post('https://script.google.com/macros/s/AKfycbzZ9fd2bjTrxX3e74Sl8GPh8D88i3GuHQlwAhWvnK8qspQknGWH0liywdzgFBh7asAw/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
-                    }
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Lo sentimos.</strong> Hay un problema con el servidor. '));
-                });
-    });
-
-});
-
 /********************** Extras **********************/
 
 // alert_markup
