60727e6ad678ed77e5e53e737a11c691c84ed22e
Jose <jguerrah@uni.pe>
2025-03-17 20:37:38 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -30,3 +30,15 @@ $(document).ready(function () {
 function alert_markup(alert_type, msg) {
     return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
 }
+
+
+/********************** Regalos **********************/
+
+function showhideclick() {
+    var x = document.getElementById("showhidetext");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
